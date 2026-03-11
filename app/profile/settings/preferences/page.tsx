4de07fa443fef66f7d83c54990d55358c8d25cc4
75781040+I0ANA24 <75781040+I0ANA24@users.noreply.github.com>
2026-03-11 11:06:22 +0200

    Quiet Hours Page

--- app/profile/settings/preferences/page.tsx
+++ app/profile/settings/preferences/page.tsx
@@ -6,21 +6,21 @@ export default function PreferencesPage() {
   return (
     <ProfilePageTemplate title="Preferences">
       <div className="w-full flex flex-col justify-center items-center gap-17">
-        <div className="w-full">
-          <h2 className="text-xl">Notifications & Alerts</h2>
+        <section className="w-full">
+          <h2 className="text-lg">Notifications & Alerts</h2>
           <div className="w-full h-px bg-[#383838] my-4"></div>
           <div className="flex flex-col justify-center items-center gap-3 mt-5">
             <ProfileSettingsLink text="quiet" />
             <ProfileSettingsLink text="distance" />
           </div>
-        </div>
-        <div className="w-full">
-          <h2 className="text-xl">App Interface</h2>
+        </section>
+        <section className="w-full">
+          <h2 className="text-lg">App Interface</h2>
           <div className="w-full h-px bg-[#383838] my-4"></div>
           <div className="flex flex-col justify-center items-center gap-3 mt-5">
             <ThemeSelector />
           </div>
-        </div>
+        </section>
       </div>
     </ProfilePageTemplate>
   );
--- app/profile/settings/preferences/quiet/page.tsx
+++ app/profile/settings/preferences/quiet/page.tsx
@@ -1,3 +1,112 @@
+"use client";
+import { useState } from "react";
+import ProfilePageTemplate from "@/components/ui/profile/ProfilePageTemplate";
+
 export default function QuietHoursPage() {
-  return <div>Quiet Hours Page</div>;
+  const [selectedCertainDays, setSelectedCertainDays] = useState<number[]>([]);
+  const [selectedAllDays, setSelectedAllDays] = useState<number[]>([]);
+
+  const days: string[] = ["M", "T", "W", "T", "F", "S", "S"];
+
+  const toggleDay = (
+    index: number,
+    selectedList: number[],
+    setList: React.Dispatch<React.SetStateAction<number[]>>,
+  ) => {
+    if (selectedList.includes(index)) {
+      setList(selectedList.filter((i) => i !== index));
+    } else {
+      setList([...selectedList, index]);
+    }
+  };
+
+  return (
+    <ProfilePageTemplate title="Quiet Hours">
+      <div className="w-full flex flex-col justify-center items-center gap-17">
+        <section className="w-full">
+          <h2 className="text-lg">Certain hours</h2>
+          <div className="w-full h-px bg-[#383838] my-4"></div>
+
+          <div className="w-full flex justify-between items-center mb-8 gap-1">
+            {days.map((day, index) => (
+              <button
+                key={`certain-${index}`}
+                onClick={() =>
+                  toggleDay(index, selectedCertainDays, setSelectedCertainDays)
+                }
+                className={`size-8 rounded-full  text-lg transition-colors duration-100 cursor-pointer ${
+                  selectedCertainDays.includes(index)
+                    ? "bg-green-light text-background"
+                    : "text-white"
+                }`}
+              >
+                {day}
+              </button>
+            ))}
+          </div>
+
+          <div className="flex w-full justify-between items-center mb-8">
+            <div className="w-55 max-w-[48%] flex flex-col gap-2">
+              <label className="text-xs tracking-wide uppercase">Start</label>
+              <input
+                type="time"
+                defaultValue="00:00"
+                className="h-13 w-full bg-secondary border border-green-light rounded-lg text-center text-2xl text-gray-400 outline-none focus:text-white"
+              />
+            </div>
+            <div className="w-55 max-w-[48%] flex flex-col gap-2">
+              <label className="text-xs tracking-wide uppercase">End</label>
+              <input
+                type="time"
+                defaultValue="12:00"
+                className="h-13 w-full bg-secondary border border-green-light rounded-lg text-center text-2xl text-gray-400 outline-none focus:text-white"
+              />
+            </div>
+          </div>
+
+          <div className="w-full flex justify-center items-center">
+            <button className="w-55 h-10 bg-green-light text-black font-medium rounded-xl cursor-pointer">
+              SAVE
+            </button>
+          </div>
+        </section>
+
+        {/* --- Secțiunea: All day --- */}
+        <section className="w-full mt-4">
+          <h2 className="text-lg">All Day</h2>
+          <div className="w-full h-px bg-[#383838] my-4"></div>
+
+          {/* Selector Zile */}
+          <div className="w-full flex justify-between items-center mb-8 gap-1">
+            {days.map((day, index) => (
+              <button
+                key={`all-${index}`}
+                onClick={() =>
+                  toggleDay(index, selectedAllDays, setSelectedAllDays)
+                }
+                className={`size-8 rounded-full  text-lg transition-colors duration-100 cursor-pointer ${
+                  selectedAllDays.includes(index)
+                    ? "bg-green-light text-background"
+                    : "text-white"
+                }`}
+              >
+                {day}
+              </button>
+            ))}
+          </div>
+
+          <div className="w-full flex justify-center items-center">
+            <button className="w-55 h-10 bg-green-light text-black font-medium rounded-xl cursor-pointer">
+              SAVE
+            </button>
+          </div>
+        </section>
+
+        {/* Footer text */}
+        <p className="text-[#808080] text-sm mt-8 leading-relaxed">
+          * Choose when to mute alerts from users in need
+        </p>
+      </div>
+    </ProfilePageTemplate>
+  );
 }
